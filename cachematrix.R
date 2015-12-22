84f801deeb57fe4c13c3bf6cc19b67b511c35b23
Ian Ferreira <ianferreira@hotmail.com>
2015-12-22 10:20:04 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+  
+{
+  ## Check if first run
+  if (!exists("cachedMatrix")){
+    cachedMatrix <<- x
+    cachedInvMatrix <<- solve(x)
+    cachedInvMatrix
+  }else if (identical(x, cachedMatrix) ){
+    ## Check if we have a cached matrix
+    cachedInvMatrix
+  }else{
+    ## Save the original matrix  
+    cachedMatrix <<- x
+    ## Save the identity matrix
+    cachedInvMatrix <<- solve(x) 
+    
+  }
+  ## return the cached inverse matrix
+  cachedInvMatrix 
+  
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  ## Return a matrix that is the inverse of 'x'
+  makeCacheMatrix(x)
 }
